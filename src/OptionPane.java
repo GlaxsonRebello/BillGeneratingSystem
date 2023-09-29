38338c1e6050bfa7c345b6395bf4544ddcb5e6ae
Glaxson <glaxsonrebello38@student.sfit.ac.in>
2023-09-29 21:36:23 +0530

    inital commit

---
+++ src/OptionPane.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+class OptionPane {
+
+    static int showshowConfirmDialog(Object object, String do_you_really_want_to_Logout_, String select, int YES_NO_OPTION) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
